e43ce24b3251b373bf2b5cdecb66672f3f388ef7
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2020-01-04 23:22:12 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.bf35aa50bba96db90c07249ee6e7c281.js"
+  "/robofriends/precache-manifest.6bcb3714566a600df03cc09515e533d2.js"
 );
 
 self.addEventListener('message', (event) => {
